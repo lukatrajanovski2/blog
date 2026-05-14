3e6a0275fac1bd7a81b09a7269f8f8d929f4491c
luka <lukatrajanovski@gmail.com>
2026-05-14 13:29:10 +0200

    Finalen dizajn so boi i fontovi

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'static'
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
