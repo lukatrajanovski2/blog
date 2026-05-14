ce0d52c0d0b7c9e4cdc2016b6d24d000ae68b7b4
luka <lukatrajanovski@gmail.com>
2026-05-14 14:14:32 +0200

    changee

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views  # ОВАА ЛИНИЈА ТИ ФАЛИ!
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
